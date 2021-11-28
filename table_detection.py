17d7d2c3ba6d2370a37b5da1e3c6cbe63851f54c
cuongdng <cuongdn.sun@gmail.com>
2021-11-28 21:07:41 +0700

    cm

--- table_detection.py
+++ table_detection.py
@@ -9,7 +9,7 @@ def table_detection(img_path, xml_path):
     img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     (thresh, img_bin) = cv2.threshold(img_gray, 128, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)
     img_bin = cv2.bitwise_not(img_bin)
-
+    #Draw bounded box from .xml data 
     tree = ET.parse(xml_path)
 
     root = tree.getroot()
@@ -29,12 +29,11 @@ def table_detection(img_path, xml_path):
     img_annotated = img.copy()
 
     for bbox in sample_annotations:
-        cv2.rectangle(img_annotated, bbox, (0, 0, 255), 1)
+        cv2.rectangle(img_annotated, (bbox[0], bbox[1]), (bbox[2], bbox[3]), (0, 0, 255), 1)
 
     cv2.imshow("img", img_annotated)
     cv2.waitKey(0)
     cv2.destroyAllWindows()
-
 
     # kernel_length_v = (np.array(img_gray).shape[1])//120
     # vertical_kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (1, kernel_length_v)) 
@@ -63,13 +62,14 @@ def table_detection(img_path, xml_path):
     #         	cv2.imwrite("./results/cropped/crop_" + str(count) + "__" + img_path.split('/')[-1], cropped)
     #     cv2.rectangle(img,(x, y),(x + w, y + h),(0, 255, 0), 1)
 
+    # cv2.imshow("img", img)
+    # cv2.waitKey(0)
+    # cv2.destroyAllWindows()
+
     # if True:    
 	#     cv2.imwrite("./results/table_detect/table_detect__" + img_path.split('/')[-1], table_segment)
 	#     cv2.imwrite("./results/bb/bb__" + img_path.split('/')[-1], img)
 
-# dir = './forms/32.png'
-# print(os.path.splitext(dir)[0])
 for i in os.listdir('./forms/'):
     if i.endswith('.png') or i.endswith('.PNG') or i.endswith('.jpg') or i.endswith('.JPG'):
         table_detection(os.path.splitext('./forms/' + i)[0] + '.png', os.path.splitext('./forms/' + i)[0] + '.xml') # os.path.splitext use for split file name out of extension
-# {table_detection('./forms/' + i, './forms/32.xml') for i in os.listdir('./forms/') if i.endswith('.png') or i.endswith('.PNG') or i.endswith('.jpg') or i.endswith('.JPG') or i.endswith('.xml') or i.endswith('.XML')}
